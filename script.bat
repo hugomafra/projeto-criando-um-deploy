ff8ed0d2e71600e3ae67ea9f90ff8254939ec03f
hugomafra <hugogm2580@gmail.com>
2023-02-12 12:39:34 -0300

    projeto deploy

---
+++ script.bat
@@ -0,0 +1,8 @@
+echo "Criando imagens..."
+
+docker build -t denilsonbonatti/projeto-backend:1.0 backend/.
+docker build -t denilsonbonatti/projeto-database:1.0 database/.
+
+echo " Realizando o push das imagens....."
+docker push denilsonbonatti/projeto-backend:1.0
+docker push denilsonbonatti/projeto-database:1.0
